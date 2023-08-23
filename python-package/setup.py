bb7e8e7f2090b09384f2a91c1ba26673cf9097ce
changchiyou <changchiyou@gmail.com>
2023-08-23 17:26:01 +0800

    chore: `0.0.0` -> `0.0.1`

--- python-package/setup.py
+++ python-package/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ptest",
-    version="0.0.0",
+    version="0.0.1",
     author="Chris",
     url="https://github.com/changchiyou/Github-Action-Test",
     description="for testing github action",
