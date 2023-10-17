b3bc2ff4974b8a173b3ae681cd4808d883771ad8
changchiyou <changchiyou@gmail.com>
2023-10-17 10:13:32 +0800

    chore: update `setup.py` with `tests_require`

--- python-package/setup.py
+++ python-package/setup.py
@@ -1,10 +1,21 @@
 from setuptools import find_packages, setup
 
+tests_require = [
+    "freezegun",
+    "pylint",
+    "mypy",
+    "pytest",
+    "pytest-xdist",
+    "pytest-console-scripts",
+    "pytest-loguru",
+]
+
 setup(
     name="ptest",
     version="0.0.0",
     author="Chris",
     url="https://github.com/changchiyou/Github-Action-Test",
+    extras_require={"test": tests_require},
     description="for testing github action",
     packages=find_packages(),
 )
