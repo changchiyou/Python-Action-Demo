2708536ec55cf4dfdb909f433f1bdd814de04887
changchiyou <changchiyou@gmail.com>
2024-06-24 21:45:24 +0800

    demo: coverage not pass

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -5,27 +5,27 @@ import pytest
 from ptest import hello, hello1, yellow
 
 
-def test_print(capsys):
-    """test ptest.hello function."""
-    hello()
+# def test_print(capsys):
+#     """test ptest.hello function."""
+#     hello()
 
-    captured = capsys.readouterr()
-    assert captured.out == "hello\n"
+#     captured = capsys.readouterr()
+#     assert captured.out == "hello\n"
 
 
-def test_print1(capsys):
-    """test ptest.hello function."""
-    hello1()
+# def test_print1(capsys):
+#     """test ptest.hello function."""
+#     hello1()
 
-    captured = capsys.readouterr()
-    assert captured.out == "hello1\n"
+#     captured = capsys.readouterr()
+#     assert captured.out == "hello1\n"
 
-def test_yellow(capsys):
-    """test ptest.hello function."""
-    yellow()
+# def test_yellow(capsys):
+#     """test ptest.hello function."""
+#     yellow()
 
-    captured = capsys.readouterr()
-    assert captured.out == "yellow\n"
+#     captured = capsys.readouterr()
+#     assert captured.out == "yellow\n"
 
 # def test_failed():
 #     """failed unit-test for testing github action workflow."""
